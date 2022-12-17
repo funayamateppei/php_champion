92906c401acff81de3fc85213ae91bdc2e0356c8
funayamateppei <t.funayama0217@gmail.com>
2022-12-18 03:49:20 +0900

    コメント追記

--- group_join_request.php
+++ group_join_request.php
@@ -7,13 +7,14 @@ session_start();
 
 require_once('./functions/connect_db.php');
 
-// 申請中のグループは参加リクエストを遅れないようにする
+// 申請中のグループは参加リクエストを送れないようにする
 $sql = 'SELECT COUNT(*) FROM group_join_request_table WHERE user_id = :user_id AND group_id = :group_id';
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':user_id', $_SESSION['id'], PDO::PARAM_INT);
 $stmt->bindValue(':group_id', $_GET['group_id'], PDO::PARAM_INT);
 $count = $stmt->fetchColumn();
 
+// もし見つからなかったらグループ参加リクエスト申請実行
 if ($count === 0) {
 $sql = 'INSERT INTO group_join_request_table (id, user_id, group_id, created_at) VALUES (NULL, :user_id, :group_id, now())';
 $stmt = $pdo->prepare($sql);
