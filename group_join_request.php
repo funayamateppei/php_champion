4789be6392815392b1ac5aed6b7e72416b6e476b
funayamateppei <t.funayama0217@gmail.com>
2022-12-18 02:46:36 +0900

    グループ参加リクエスト機能実装

--- group_join_request.php
+++ group_join_request.php
@@ -3,4 +3,14 @@
 // var_dump($_GET['group_id']);
 // exit();
 
+require_once('./functions/connect_db.php');
+
+$sql = 'INSERT INTO group_join_request_table (id, user_id, group_id, created_at) VALUES (NULL, :user_id, :group_id, now())';
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':user_id', $_SESSION['id'], PDO::PARAM_INT);
+$stmt->bindValue(':group_id', $_GET['group_id'], PDO::PARAM_INT);
+$stmt->execute();
+
+header('Location:./group_select.php');
+
 ?>
