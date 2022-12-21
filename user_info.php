6748a8fd11816ef0615832053bb854d2fad29d53
funayamateppei <t.funayama0217@gmail.com>
2022-12-21 14:45:39 +0900

    user_info.php css 作成

--- user_info.php
+++ user_info.php
@@ -25,22 +25,39 @@ $_SESSION['id'] = $row['id'];
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/earlyaccess/kokoro.css" rel="stylesheet">
+  <link rel="stylesheet" href="./user_info.css">
   <title>Document</title>
 </head>
 
 <body>
+  <h1>User Info Form</h1>
+
   <fieldset>
-    <legend>ユーザー情報登録</legend>
     <form action="./user_info_create.php" method="POST">
 
-      <label for="lastName">苗字</label>
-      <input id="lastName" class="input" type="text" name="lastName">
+      <div class="div">
+        <label for="lastName">苗字</label>
+        <input id="lastName" class="input" type="text" name="lastName">
+      </div>
 
-      <label for="firstName">名前</label>
-      <input id="firstName" class="input" type="text" name="firstName">
+      <div class="div">
+        <label for="firstName">名前</label>
+        <input id="firstName" class="input" type="text" name="firstName">
+      </div>
 
-      <input type="radio" name="gender" value="0">男性
-      <input type="radio" name="gender" value="1">女性
+      <div class="gender">
+        <div>
+          <input id="man" class="radio" type="radio" name="gender" value="0">
+          <label class="label" for="man">男性</label>
+        </div>
+
+        <div>
+          <input id="woman" class="radio" type="radio" name="gender" value="1">
+          <label class="label" for="woman">女性</label>
+        </div>
+      </div>
 
       <button>登録</button>
 
